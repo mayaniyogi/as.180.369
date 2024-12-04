abc37000d387408c5601203e35de492c5e28e42b
mayaniyogi <maya.niyogi.1@gmail.com>
2024-12-04 10:49:33 -0500

    fixing reproduce.sh

--- contrib/mayaniyogi/Paper/binder/reproduce.sh
+++ contrib/mayaniyogi/Paper/binder/reproduce.sh
@@ -1,14 +1,3 @@
-d#!/bin/bash
-
-# Step 1: Create the Conda environment from the environment.yml file
-conda env create -f environment.yml
-
-# Step 2: Activate the Conda environment
-source activate ./econ_ark  # Replace "myenv" with the name of your environment if different
-
-# Step 3: Run the Jupyter notebook to generate outputs (in-place execution)
-jupyter nbconvert --to notebook --execute --inplace ../Working_notebook.ipynb
-
 #!/bin/bash
 # Create the environment
 read -r -p "Do you want to create/update your environment? [y/n]" input
